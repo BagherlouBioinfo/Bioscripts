6a0b4fb9ebfad0891b18ee991e0731a56a0d383f
Dr. Nazanin Bagherlou <nznnbagherlou@gmail.com>
2025-06-14 20:32:01 +0200

    Update S2.py

--- Script-ScRNA-seq-Analysis/S2.py
+++ Script-ScRNA-seq-Analysis/S2.py
@@ -1,18 +1,18 @@
-# S-2_Label_DPSC_PDLSC.py
+# Step2_Label_DPSC_PDLSC.py
 
 import scanpy as sc
 
 # Load the filtered AnnData object
-adata = sc.read("G:/Dental Project/Processed/adata_dental_raw_filtered.h5ad")
+adata = sc.read("./Dental Project/Processed/adata_dental_raw_filtered.h5ad")
 
 # Assign cell type based on whether the cell name includes "DPSC"
 adata.obs['CellSource'] = adata.obs_names.to_series().apply(lambda x: 'DPSC' if 'DPSC' in x else 'PDLSC')
 
 # Show summary of cell counts per group
-print("✅ Cell counts per group:")
+print("Cell counts per group:")
 print(adata.obs['CellSource'].value_counts())
 
 # Save updated AnnData object
 adata.write("G:/Dental Project/Processed/adata_dental_labeled.h5ad")
 
-print("🎯 Labeled AnnData saved with 'CellSource' column.")
+print("Labeled AnnData saved with 'CellSource' column.")
